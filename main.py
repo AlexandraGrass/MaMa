0520c685df8bebdd863a8784cd6948fe868dac17
Raymond Pinto <ge73lam@mytum.de>
2019-07-24 15:17:52 +0200

    Code generation done for func app, let_rec and uop. Also modified if

--- main.py
+++ main.py
@@ -64,5 +64,14 @@ if __name__ == '__main__':
     #parse_syntaxTree(result)
     #result = parse_tree("let a = 17 in let f = fun b -> a + b in f (39 + 2)")
     #parse_syntaxTree(result)
-    result = parse_tree("let a = 17 in let f = fun b -> a + b in f 42") # 140
-    parse_syntaxTree(result)
+    #result = parse_tree("let a = 17 in let f = fun b -> a + b in f 42") # 140
+    #parse_syntaxTree(result)
+    #result = parse_tree("let rec f = fun x y -> if y <= 1 then x else f ( x * y ) ( y - 1 ) in f 1")
+    #parse_syntaxTree(result)
+    #result = parse_tree("let x3 = 4 in     +     x3    ")
+    #parse_syntaxTree(result)
+    #result = parse_tree("let f = fun x y -> let a = 5 in let b = x + 2*y in b + a*x in f 0 1")
+    #parse_syntaxTree(result)
+    #result = parse_tree("if y > x then x else 7 + y * x")
+    #parse_syntaxTree(result)
+
